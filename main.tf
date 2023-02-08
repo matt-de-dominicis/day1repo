eeabc329eb8a3a937cab0d64f957241a111d8614
matt-de-dominicis <124634227+matt-de-dominicis@users.noreply.github.com>
2023-02-08 15:58:46 -0500

    Added Azure resource group and two storage accounts

--- main.tf
+++ main.tf
@@ -6,8 +6,32 @@ provider "azurerm" {
   }
 }
 
-resource "azurerm_resource_group" "day2_rg" {
-  name     = "day2_rg"
-  location = "West Europe"
-  tags = { environment = "test" }
+resource "azurerm_resource_group" "day3_rg" {
+  name     = "example"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "day3-storage-acct-1" {
+  name                     = "day3exercisestorageacct1"
+  resource_group_name      = azurerm_resource_group.day3_rg.name
+  location                 = azurerm_resource_group.day3_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    storage = "first-sa"
+  }
+}
+
+resource "azurerm_storage_account" "day3-storage-acct-2" {
+  name                     = "day3exercisestorageacct2"
+  resource_group_name      = azurerm_resource_group.day3_rg.name
+  location                 = azurerm_resource_group.day3_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  depends_on = [azurerm_storage_account.day3-storage-acct-1]
+
+  tags = {
+    storage = "second-sa"
+  }
 }
